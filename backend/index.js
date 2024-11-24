7d3e5b34d3037bfebff7ad458530afaa4318009d
Hery1235 <mh47098@gmail.com>
2024-11-24 17:55:17 +0000

    ddsdff

--- backend/index.js
+++ backend/index.js
@@ -18,11 +18,14 @@ const Users = require("./models/userModel");
 const Orders = require("./models/orderModel");
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: "http://localhost:5173", // Your frontend URL (localhost:5173 for development)
-  })
-);
+const corsOptions = {
+  origin: "http://localhost:5173", // Allow only your frontend
+  methods: "GET, POST, PUT, DELETE", // Allowed HTTP methods
+  allowedHeaders: "Content-Type, Authorization", // Allowed headers
+  credentials: true, // Allow cookies if needed
+};
+
+app.use(cors(corsOptions));
 
 // Order place function to database
 const placeOrder = async (
